4321a0ce8d6699541d46b1755c34ce081b102cb9
danielgoqueiroz <contato@danielqueiroz.com>
2023-02-22 21:07:54 -0300

    Ajuste de mongo

--- src/main/java/com/danielqueiroz/fotoradar/FotoradarApplication.java
+++ src/main/java/com/danielqueiroz/fotoradar/FotoradarApplication.java
@@ -1,11 +1,5 @@
 package com.danielqueiroz.fotoradar;
 
-import com.danielqueiroz.fotoradar.exception.AlreadyExistException;
-import com.danielqueiroz.fotoradar.exception.ValidationException;
-import com.danielqueiroz.fotoradar.model.Role;
-import com.danielqueiroz.fotoradar.model.User;
-import com.danielqueiroz.fotoradar.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -14,13 +8,10 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.Collections;
-
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class FotoradarApplication {
 
-	public static void main(String[] args) throws ValidationException, AlreadyExistException {
-
+	public static void main(String[] args) {
 		SpringApplication.run(FotoradarApplication.class, args);
 	}
 
